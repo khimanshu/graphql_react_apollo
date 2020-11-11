9a27cd69509bc460cb080dc19ad9c7e3ec84e093
Himanshu Koyalkar <himanshu.koyalkar@nike.com>
2020-11-11 04:09:58 -0800

    Code with connections

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import BookList from "./BookList";
+
+
+// Pass your GraphQL endpoint to uri (since our express graphQL endpoint is running at localhost:4000)
+const client = new ApolloClient({ uri: 'https://localhost:4000/graphql' });
+
+// We are trying to get Booklist and send it in ApolloProvider
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+                <ApolloProvider client={client}>
+                    <BookList/>
+                </ApolloProvider>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+import {ApolloProvider} from "react-apollo";
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
+// GraphQL query to fetch all data from graphQL server at localhost:4000
+const GET_BOOKS = gql`
+    query {
+      books {
+        id,
+        title,
+        available
+        }
+    }
+`;
+
+// Components render views
+// This component runs the above GET_BOOKS query
+class BookList extends Component {
+    render() {
+        return (
+            <Query query={GET_BOOKS}>
+                {({ loading, error, data }) => {
+                    if (loading) {
+                        return <div>Loading</div>
+                    }
+
+                    if (error) {
+                        return <div>Error {error.toString()}</div>
+                    }
+
+                    console.log(data);
+
+                    return (
+                        <ul>
+                            {
+                            return data.books.map((b) => {
+                                return <div key={b.id}>{b.title}</div>
+                            })
+                            }
+                        </ul>
+                    )
+                }}
+            </Query>
+        );
+    }
+}
+
+export default BookList;
